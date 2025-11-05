d0fb700006d5184badaaedc2a7ec47b0e4c7567e
Aljuhaymi <s202261920@ccm.kfupm.edu.sa>
2025-11-05 09:06:48 +0300

    finsihed task 1

--- 5-7-node-express/backend/server.js
+++ 5-7-node-express/backend/server.js
@@ -2,7 +2,14 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
+{/*write code for cors*/
+    const app = express();
+    const PORT = 3000;
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+
+}
 
 
 {/*write code to define routes*/}
